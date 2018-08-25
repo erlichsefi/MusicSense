07fc8639fa64761082bca4669d7773f5aca20785
mariavinogradov <maria.vinog@gmail.com>
2018-08-25 20:58:59 +0300

    Code refactor

--- app/src/main/java/metaextract/nkm/com/myplayer/ArithmeticFunctions.java
+++ app/src/main/java/metaextract/nkm/com/myplayer/ArithmeticFunctions.java
@@ -31,6 +31,26 @@ public class ArithmeticFunctions {
         else return 0;
     }
 
+    public double Max(Vector<Double> vec){
+        if(vec == null) return 0;
+        double max = vec.elementAt(0);
+        for (double d :
+                vec) {
+            if (d > max) max = d;
+        }
+        return max;
+    }
+
+    private double Min(Vector<Double> vec) {
+        if(vec == null) return 0;
+        double min = vec.elementAt(0);
+        for (double d :
+                vec) {
+            if (d < min) min = d;
+        }
+        return min;
+    }
+
     public MyLambdaFunction getAvg() {
         MyLambdaFunction Avg = (vec) -> {
             if(vec == null) return 0;
@@ -50,4 +70,21 @@ public class ArithmeticFunctions {
         };
         return Sd;
     }
+
+    MyLambdaFunction getMax(){
+        MyLambdaFunction max = (vec) -> {
+            double res = Max(vec);
+            return res;
+        };
+        return max;
+    }
+
+    MyLambdaFunction getMin(){
+        MyLambdaFunction min = (vec) -> {
+           double res = Min(vec);
+          return res;
+        };
+        return min;
+    }
+
 }
