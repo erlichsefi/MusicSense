ddcaa9eb8d0466a5d9d9c3e9d2eb708cab9b574c
mariavinogradov <maria.vinog@gmail.com>
2018-08-25 11:12:22 +0300

    Code refactor

--- app/src/main/java/metaextract/nkm/com/myplayer/ActivityReader.java
+++ app/src/main/java/metaextract/nkm/com/myplayer/ActivityReader.java
@@ -3,57 +3,73 @@ package metaextract.nkm.com.myplayer;
 import java.io.*;
 import java.util.Vector;
 
-public class ActivityReader {
+import static metaextract.nkm.com.myplayer.FileManager.getPublicPicturesDirectory;
 
-    private String [] LastLine = null;
+class ActivityReader {
 
-    /*
-    public ActivityReader(String path){
-        File input = new File(path);
+    private String LastLine = null;
+
+    ActivityReader(String StartingTime) {
+        File f = getPublicPicturesDirectory("Log");
+        File input = new File(f, "Activity.csv");
+        String Time;
         try {
             FileReader fr = new FileReader(input);
             BufferedReader br = new BufferedReader(fr);
             br.readLine();
             br.readLine();
             br.readLine();
-            LastLine = br.readLine().split(",");
-            br.close();
-            fr.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            String row = br.readLine();
+            if (row != null) {
+                String[] split = row.split(",");
+                Time = split[1];
+                String[] regTime = Time.split(":");
+                double convertedCurrentTime = Double.parseDouble(regTime[0] + regTime[1] + regTime[2]);
+                String[] regStart = StartingTime.split(":");
+                double convertedStartingTime = Double.parseDouble(regStart[0] + regStart[1] + regStart[2]);
+
+                //find the correct line to start reading from
+                while (convertedCurrentTime < convertedStartingTime) {
+                    row = br.readLine();
+                    if (row == null) {
+                        LastLine = null;
+                        br.close();
+                        fr.close();
+                        return;
+                    }
+                }
+            }
+            LastLine = row;
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    */
-
-    public ActivityReader(String Time){
-        ReadFileCSV read = new ReadFileCSV();
-        String [][] action = read.ReadActivity(Time);
-        if(action != null)LastLine = action[0];
-        else LastLine = null;
-    }
 
 
     //iterate over all the lines in activity.csv, and extract vectors. keep the last line
-    public void extractData(){
-        FileManager fm = new FileManager("DataVector",true);
+    void extractData() {
+        FileManager fm = new FileManager("DataVector", true);
         DataVector dv = new DataVector();
         ReadFileCSV read = new ReadFileCSV();
-        String [][] action = read.ReadActivity(LastLine[1]);
-        while(action != null){ //while not EOF
-            LastLine = action[1];
-            dv.GetDataFromSensors(action[0][3],action[0][1],action[1][1]);
-            Vector<Double> ProcessedData = dv.getFinalVector();
-            if (ProcessedData.size() != 0){
-                fm.writeInternalFileCsvNewLINE(String.valueOf(ProcessedData.elementAt(0)),true);
-                for (int i = 1; i < ProcessedData.size();i++ ) {
-                    fm.writeInternalFileCsvSameLine(String.valueOf(ProcessedData.elementAt(i)),true);
+        if (LastLine != null) {
+            String[] twoActivityLines = read.ReadActivity(LastLine);
+            while (twoActivityLines != null && twoActivityLines[1] != null) { //while not EOF || file is not empty
+                LastLine = twoActivityLines[1];
+                String[][] action = new String[2][];
+                action[0] = twoActivityLines[0].split(",");
+                action[1] = twoActivityLines[1].split(",");
+                dv.GetDataFromSensors(action[0][3], action[0][1], action[1][1]);
+                Vector<Double> ProcessedData = dv.getFinalVector();
+                if (ProcessedData.size() != 0) {
+                    fm.writeInternalFileCsvNewLINE(String.valueOf(ProcessedData.elementAt(0)), true);
+                    for (int i = 1; i < ProcessedData.size(); i++) {
+                        fm.writeInternalFileCsvSameLine(String.valueOf(ProcessedData.elementAt(i)), true);
+                    }
+                    fm.writeInternalFileCsvSameLine(action[1][7], true);
                 }
-                fm.writeInternalFileCsvSameLine(action[1][7],true);
+                twoActivityLines = read.ReadActivity(LastLine);
             }
-            System.out.println();
-            action = read.ReadActivity(LastLine[1]);
         }
     }
 
--- app/src/main/java/metaextract/nkm/com/myplayer/ReadFileCSV.java
+++ app/src/main/java/metaextract/nkm/com/myplayer/ReadFileCSV.java
@@ -2,16 +2,15 @@ package metaextract.nkm.com.myplayer;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Vector;
 
 import static metaextract.nkm.com.myplayer.FileManager.getPublicPicturesDirectory;
 
-public class ReadFileCSV {
+class ReadFileCSV {
 
-    public Vector<Double> ReadSensor(String FileName, int column, String StartingTime, String finishTime) {
+    Vector<Double> ReadSensor(String FileName, int column, String StartingTime, String finishTime) {
         Vector<Double> data = new Vector<>();
         File input = new File(FileName);
         String Time;
@@ -21,7 +20,7 @@ public class ReadFileCSV {
             br.readLine();
             br.readLine();
             String row = br.readLine();
-            if(row == null){
+            if (row == null) {
                 br.close();
                 fr.close();
                 return null;
@@ -32,17 +31,17 @@ public class ReadFileCSV {
                 String convertedStartingTime;
                 String convertedFinishTime;
                 String convertedCurrentTime;
-                String [] split1 = StartingTime.split(":");
-                convertedStartingTime = split1[0]+split1[1]+split1[2];
-                String [] split2 = Time.split(":");
-                convertedCurrentTime = split2[0]+split2[1]+split2[2];
-                String [] split3 = finishTime.split(":");
-                convertedFinishTime = split3[0]+split3[1]+split3[2];
+                String[] split1 = StartingTime.split(":");
+                convertedStartingTime = split1[0] + split1[1] + split1[2];
+                String[] split2 = Time.split(":");
+                convertedCurrentTime = split2[0] + split2[1] + split2[2];
+                String[] split3 = finishTime.split(":");
+                convertedFinishTime = split3[0] + split3[1] + split3[2];
 
                 //find the correct line to start reading from
                 while (Double.parseDouble(convertedCurrentTime) < Double.parseDouble(convertedStartingTime)) {
                     row = br.readLine();
-                    if(row == null){
+                    if (row == null) {
                         br.close();
                         fr.close();
                         return null;
@@ -55,29 +54,29 @@ public class ReadFileCSV {
                     }
                     Time = split[1];
                     split2 = Time.split(":");
-                    if(split2.length < 3){
+                    if (split2.length < 3) {
                         break;
                     }
-                    convertedCurrentTime = split2[0]+split2[1]+split2[2];
+                    convertedCurrentTime = split2[0] + split2[1] + split2[2];
                 }
 
                 //start reading and pushing data to the vector until the finish time
                 while (Double.parseDouble(convertedCurrentTime) <= Double.parseDouble(convertedFinishTime)) {
                     data.add(Double.parseDouble(split[column]));
                     row = br.readLine();
-                    if(row == null){
+                    if (row == null) {
                         break;
                     }
                     split = row.split(",");
-                    if(split.length < 3){
+                    if (split.length < 3) {
                         break;
                     }
                     Time = split[1];
                     split2 = Time.split(":");
-                    if(split2.length < 3){
+                    if (split2.length < 3) {
                         break;
                     }
-                    convertedCurrentTime = split2[0]+split2[1]+split2[2];
+                    convertedCurrentTime = split2[0] + split2[1] + split2[2];
                 }
 
 
@@ -85,8 +84,6 @@ public class ReadFileCSV {
 
             br.close();
             fr.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -95,11 +92,10 @@ public class ReadFileCSV {
         return data;
     }
 
-    public String[][] ReadActivity(String StartingTime) {
+    String[] ReadActivity(String rowToStart) {
         File f = getPublicPicturesDirectory("Log");
-        File input = new File(f,"Activity.csv");
-        String[][] res = new String[2][];
-        String Time;
+        File input = new File(f, "Activity.csv");
+        String[] res = new String[2];
         try {
             FileReader fr = new FileReader(input);
             BufferedReader br = new BufferedReader(fr);
@@ -107,43 +103,19 @@ public class ReadFileCSV {
             br.readLine();
             br.readLine();
             String row = br.readLine();
-            String[] split = row.split(",");
-            if (split.length > 1) {
-                Time = split[1];
-                String [] regTime = Time.split(":");
-                double convertedCurrentTime = Double.parseDouble(regTime[0]+regTime[1]+regTime[2]);
-                String [] regStart = StartingTime.split(":");
-                double convertedStartingtime = Double.parseDouble(regStart[0]+regStart[1]+regStart[2]);
-
-                //find the correct line to start reading from
-                while (convertedCurrentTime < convertedStartingtime) {
-                    row = br.readLine();
-                    if(row == null) return null;
-                    split = row.split(",");
-                    if (split.length < 1) {
-                        br.close();
-                        fr.close();
-                        return null;
-                    }
-                    Time = split[1];
-                    regTime = Time.split(":");
-                    convertedCurrentTime = Double.parseDouble(regTime[0]+regTime[1]+regTime[2]);
-                }
+            while (!row.equals(rowToStart)) {
+                row = br.readLine();
+                if (row == null) return null;
             }
 
-            res[0] = split;
+            res[0] = row;
             row = br.readLine();
             br.close();
             fr.close();
-            if(row == null){
-                return null;
-            }
-            split = row.split(",");
-            if (split.length < 1){
+            if (row == null) {
                 res[1] = null;
                 return res;
-            }
-            res[1] = split;
+            } else res[1] = row;
 
 
         } catch (IOException e) {
