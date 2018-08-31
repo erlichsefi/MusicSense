1e5092e4e379bface330ad645f957e41789b0e62
Maria Vinogradov <mariav@jfrog.com>
2018-08-31 23:18:52 +0300

    typo

--- app/src/main/java/metaextract/nkm/com/myplayer/MainActivity.java
+++ app/src/main/java/metaextract/nkm/com/myplayer/MainActivity.java
@@ -553,7 +553,7 @@ public class MainActivity extends Activity implements OnCompletionListener, Seek
         startActivityForResult(i, 100);
     }
 
-    public void gspClick(View view) {
+    public void gpsClick(View view) {
         Intent i = new Intent(getApplicationContext(), GpsService.class);
         startActivityForResult(i, 100);
     }
